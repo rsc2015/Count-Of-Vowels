dff3e3fc568573e17629430b7f4976213999d3a4
Rohini <rohinichavan@gmail.com>
2018-01-21 14:39:20 -0600

    Update vowels-1.js

--- vowels-1.js
+++ vowels-1.js
@@ -3,10 +3,7 @@ let thePhrase = document.getElementById("vowels").innerText;
 thePhrase = thePhrase.toLowerCase();
 console.log(thePhrase);
 
-    //var a=document.getElementById("t");
-    console.log(thePhrase); //to see input string on console
-    n = thePhrase.length;
-    // console.log(n); //calculated length of string
+    
   
      let theVowels = ["a", "e", "i", "o", "u"];
     var c = 0;
